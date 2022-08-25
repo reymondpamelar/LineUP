93b7666b7ed1dd15a17e26e824023b2714dc13a6
reymondpamelar <reymondpamelar@gmail.com>
2022-08-25 15:48:55 -0700

    preset step finished

--- src/containers/signup-steps/style-screen.js
+++ src/containers/signup-steps/style-screen.js
@@ -3,16 +3,15 @@ import * as Icon from "react-feather";
 import {useEffect, useState} from "react";
 import ContactFormScreen from "../../containers/signup-steps/contact-form-screen";
 import FlipMove from "react-flip-move";
-import Overview from "./style-screen/overview";
-import Lottie from "react-lottie";
-import animationData from "../../assets/circular-line.json";
+import Preset from "./style-screen/preset";
 
 export default function StyleScreen(props) {
     const [step, setStep] = useState(0)
+    const [preset, setPreset] = useState('')
 
     const sections = [
-        {index: 0, title:'vision', content: <Overview firstName={props.firstName} setStep={setStep}/>},
-        {index: 1, title:'fade', content: <Overview firstName={props.firstName} setStep={setStep}/>},
+        {index: 0, title:'vision', content: <Preset firstName={props.firstName} setStep={setStep} setPreset={setPreset}/>},
+        {index: 1, title:'fade', content: <Preset firstName={props.firstName} setStep={setStep}/>},
     ]
 
     const customEnterAnimation = {
@@ -23,8 +22,19 @@ export default function StyleScreen(props) {
         from: { transform: 'translateX(0%)', opacity:'1' },
         to: { transform: 'translateX(-6%)', opacity:'0'},
     };
+
+    useEffect(() => {
+        console.log(preset)
+    },[preset])
+
+    function nextStep(){
+        setTimeout(() => {
+            setStep(step + 1)
+        },0)
+    }
+
     return (
-        <div>
+        <div className={'px-20'}>
             <FlipMove enterAnimation={customEnterAnimation} leaveAnimation={customLeaveAnimation} typeName={null}>
                 {sections.map((page)=>{
                     if(step >= page.index){
@@ -36,6 +46,12 @@ export default function StyleScreen(props) {
                     }
                 })}
             </FlipMove>
+            <div className={'flex justify-end mt-10'}>
+                <button onClick={() => nextStep()} className={'gap-4 flex items-center rounded-xl bg-[#D58258] hover:bg-black text-white p-4 px-10 animate__animated animate__fadeInDown transition ease-in-out duration-200'}>
+                    Next
+                    <Icon.ChevronRight className={'w-5'}/>
+                </button>
+            </div>
         </div>
     );
 }
---
+++ src/containers/signup-steps/style-screen/preset.js
@@ -0,0 +1,60 @@
+import TextInput from "../../../components/UI/Form/text-input";
+import * as Icon from "react-feather";
+
+export default function Preset (props) {
+
+
+    return (
+        <div className={''}>
+            <div>
+                <h1 className={'relative capitalize'}>
+                    Hello {props.firstName}!
+                </h1>
+            </div>
+            <div>
+                <h2 className={'relative mt-10'}>
+                    What are you in for?
+                </h2>
+                <form onChange={(e) => props.setPreset(e.target.value)} className={' mt-10 gap-10 flex justify-center flex-wrap'}>
+                    <label htmlFor={'shave'} className={'hover:scale-110 transition ease-in-out duration-400 hover:shadow-lg'}>
+                        <div className={'cursor-pointer flex justify-center items-center w-32 h-32 rounded-t-xl overflow-hidden border'}>
+                            <img className={'object-cover'} src={'bald.png'}/>
+                        </div>
+                        <input onChange={(e) => props.setPreset(e.target.value)} value='shave' name="preset" id="shave" type="radio" className={'hidden peer'} />
+                        <div className={'bg-black peer-checked:bg-[#ECB984] rounded-b-xl text-white p-2 transition ease-in-out duration-500'}>
+                            Shave
+                        </div>
+                    </label>
+                    <label htmlFor={'trim'} className={'hover:scale-110 transition ease-in-out duration-400 hover:shadow-lg'}>
+                        <div className={'cursor-pointer flex justify-center items-center w-32 h-32 rounded-t-xl overflow-hidden border'}>
+                            <img className={'w-16 object-cover'} src={'scissors.png'}/>
+                        </div>
+                        <input onChange={(e) => props.setPreset(e.target.value)} value='trim' name="preset" id="trim" type="radio" className={'hidden peer'} />
+                        <div className={'bg-black peer-checked:bg-[#ECB984] rounded-b-xl text-white p-2 transition ease-in-out duration-500'}>
+                            Trim
+                        </div>
+                    </label>
+                    <label htmlFor={'fade'} className={'hover:scale-110 transition ease-in-out duration-400 hover:shadow-lg rounded-xl'}>
+                        <div className={'p-2 cursor-pointer flex justify-center items-center w-32 h-32 rounded-t-xl overflow-hidden border'}>
+                            <img className={'object-cover'} src={'fade.png'}/>
+                        </div>
+                        <input onChange={(e) => props.setPreset(e.target.value)} value='fade' name="preset" id="fade" type="radio" className={'hidden peer'} />
+                        <div className={'bg-black peer-checked:bg-[#ECB984] rounded-b-xl text-white p-2 transition ease-in-out duration-500'}>
+                            Fade
+                        </div>
+                    </label>
+                    <label htmlFor={'trim + fade'} className={'hover:scale-110 transition ease-in-out duration-400 hover:shadow-lg rounded-xl'}>
+                        <div className={'p-2 cursor-pointer grid grid-cols-2 justify-center items-center w-32 h-32 rounded-t-xl overflow-hidden border'}>
+                            <img className={'w-10 ml-2 object-cover'} src={'scissors.png'}/>
+                            <img className={'object-cover'} src={'fade.png'}/>
+                        </div>
+                        <input onChange={(e) => props.setPreset(e.target.value)} value='trim + fade' name="preset" id="trim + fade" type="radio" className={'hidden peer'} />
+                        <div className={'bg-black peer-checked:bg-[#ECB984] rounded-b-xl text-white p-2 transition ease-in-out duration-500'}>
+                            Trim + Fade
+                        </div>
+                    </label>
+                </form>
+            </div>
+        </div>
+    )
+}
